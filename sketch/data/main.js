ab5485bc6c7cfa6dd1c82324da3f2aba5759e876
lauroro <88981092+lauroro@users.noreply.github.com>
2022-07-27 11:21:39 +0200

    add LittleFS, rebuild web-interface

---
+++ sketch/data/main.js
@@ -0,0 +1,73 @@
+
+var tValue;
+var hValue;
+var label;
+var labelsNum = 0;
+var ctx = document.getElementById('chart').getContext('2d');   
+
+var chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    datasets:[
+      {
+        label: 'Temperature',
+        borderColor: 'rgb(255, 38, 38)',
+      },
+      {
+        label: 'Humidity',
+        borderColor: 'rgb(51, 80, 255)',
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    responsive:true,
+    y: {
+      beginAtZero: true
+    }
+  }
+});
+
+setInterval(function ( ) {
+  // https://www.w3schools.com/xml/xml_http.asp
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('temperature').innerHTML = this.responseText;
+      tValue = this.responseText;
+    }
+  };
+  xhttp.open('GET', '/temperature', true);
+  xhttp.send();
+}, 5000);
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('humidity').innerHTML = this.responseText;
+      hValue = this.responseText;
+    }
+  };
+  xhttp.open('GET', '/humidity', true);
+  xhttp.send();
+}, 5000);
+
+setInterval(function(){
+  updateChart();
+},5000);
+
+function updateChart(){
+  if(labelsNum == 20){
+    chart.data.labels.shift();
+    chart.data.datasets[0].data.shift();
+    chart.data.datasets[1].data.shift();
+  }
+  else{
+    labelsNum++;
+  }
+  chart.data.labels.push(new Date().toLocaleTimeString());
+  chart.data.datasets[0].data.push(tValue);
+  chart.data.datasets[1].data.push(hValue);
+  chart.update();
+}
